24c810e708e4e1841c43704f7d975216832b5b15
linhtd <linh.tdb@teko.vn>
2021-06-27 00:16:26 +0700

    correct place application layer

---
+++ server/controller/seller.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"coding-challenge-go/pkg/seller"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
+
+func NewSellerController(sellerSvc seller.Service) *sellerController {
+	return &sellerController{
+		sellerSvc: sellerSvc,
+	}
+}
+
+type sellerController struct {
+	sellerSvc seller.Service
+}
+
+func (sc *sellerController) List(c *gin.Context) {
+	sellers, err := sc.sellerSvc.List(c.Request.Context())
+
+	if err != nil {
+		log.Error().Err(err).Msg("Fail to query seller list")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Fail to query seller list"})
+		return
+	}
+
+	sellersJson, err := json.Marshal(sellers)
+
+	if err != nil {
+		log.Error().Err(err).Msg("Fail to marshal sellers")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Fail to marshal sellers"})
+		return
+	}
+
+	c.Data(http.StatusOK, "application/json; charset=utf-8", sellersJson)
+}
